4e91d05465bb36c5458d1d1ec791969611424153
Marvin <okongomarvin971@gmail.com>
2024-09-09 14:03:18 +0300

    authenticate 1

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
+import {Logo} from "../assets";
+import { Footer } from '../containers';
+
 function Authentication() {
   return (
-    <div>Authentication</div>
+    <div className='auth-section'>
+        {/* top section */}
+        <img src={Logo} alt='' className='w-12 h-auto object-contain'/>
+
+        {/* main section */}
+        <div className="w-full flex flex-1 flex-col items-center justify center gap-6">
+            <h1 className='text-3xl lg:text-4xl text-blue-700'>Welcome to Expressume</h1>
+            <p className='text-base text-gray-600'>Express way to creeate resume</p>
+            <h2 className='text-2xl text-gray-600'>Authenticate</h2>
+
+            <div className=""></div>
+        </div>
+
+        {/* footer section */}
+        <Footer />
+    </div>
   )
 }
-
+ 
 export default Authentication
